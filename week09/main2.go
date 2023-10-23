3aa8f9554c50cbe9d4673c840584ae391ee0115e
Elsa Balqish <elsabalqish@gmail.com>
2023-10-23 12:12:24 +0900

    week09 v1.8

---
+++ week09/main2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func swap(n1 *int, n2 *int) {
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
+
+func main() {
+	a := 10
+	b := 20
+
+	c := &a
+	fmt.Printf("%T\n", c)
+
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
+}
