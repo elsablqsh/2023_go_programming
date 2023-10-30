fcafba59b9a1a31852aac7d6e97681b9b9706fe5
Elsa Balqish <elsabalqish@gmail.com>
2023-10-30 11:45:14 +0900

    week10 v2.2

--- week10/src/mymath/mymath.go
+++ week10/src/mymath/mymath.go
@@ -1,5 +1,13 @@
 package mymath
 
+func MyPower(base int, exponent int) int {
+	result := 1
+	for i := 1; i <= exponent; i++ {
+		result = result * base
+	}
+	return result
+}
+
 func MyAbs(number int) int { //value function !!!
 	if number < 0 {
 		return number * -1
