e045d468cb7562c7d66aab3b9dd3a6fcd2abbd22
Elsa Balqish <elsabalqish@gmail.com>
2023-11-13 12:01:12 +0900

    week12 v3.7

--- week12/main.go
+++ week12/main.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 	a := make([]string, 4, 5)
-	//a := [] string{"a", "b", "c", "d"}
 	a[0] = "a"
 	a[2] = "c"
 	a[3] = "d"
 	as := a[0:2]
+	c := append(a, "y", "x")
+	//c := append(a, "y")
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(c, len(c), cap(c))
 	as[1] = "z"
-	fmt.Println(a, len(a), cap(a))
-	fmt.Printf("%x %x %x\n", &a[0], &as[0], &a[1])
+	//fmt.Printf("%x %x %x\n", &a[0], &as[0], &a[1])
+	//fmt.Println(a, len(a), cap(a))
 }
