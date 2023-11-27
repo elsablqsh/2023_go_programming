a28a04596a6378b565bb052c9e7737c592323a50
Elsa Balqish <elsabalqish@gmail.com>
2023-11-27 11:32:42 +0900

    week14 v4.6

--- week14/main.go
+++ week14/main.go
@@ -3,24 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	//var games map[int]string
-	// games = make(map[int]string)
-
-	games := map[int]string{
-		427: "elsa",
-		251: "su",
-		108: "kamal",
-		106: "victor",
-		408: "yujin"}
-	// append
-
-	for _, v := range games {
-		fmt.Println(v)
-	}
-	games[312] = "tae" // update
-	delete(games, 408) // delete
-
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	//balls := make(map[string]int)
+	var balls map[string]int
+	fmt.Printf("%#v\n", balls)
+	balls["elsa"] = 27
+	fmt.Println(balls["elsa"])
+	fmt.Println(balls["su"])
 }
