48119dd2793827413a9596316b10fc1d8facbdcf
Elsa Balqish <elsabalqish@gmail.com>
2023-11-27 11:23:32 +0900

    week14 v4.3

---
+++ week14/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var games map[int]string
+	games = make(map[int]string)
+	// append
+	games[427] = "elsa"
+	games[251] = "su"
+	games[108] = "kamal"
+	games[106] = "victor"
+	games[408] = "yujin"
+
+	for _, v := range games {
+		fmt.Println(v)
+	}
+	games[312] = "tae" // update
+	delete(games, 408) // delete
+
+	for k, v := range games {
+		fmt.Println(k, v)
+	}
+}
